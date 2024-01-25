b1d9a773c8891b686b0b4619de140b19f0219044
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-25 18:22:28 +0100

    add header and footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2>footer</h2>
+            </div>
+        </div>
+    </div>
+</footer>
