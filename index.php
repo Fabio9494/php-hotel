9c4dd921d8bc911fce30ac724e5a86c58c87c752
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-25 18:05:58 +0100

    created table

--- index.php
+++ index.php
@@ -39,10 +39,39 @@
         ],
 
     ];
-
-    foreach($hotels as $hotel ){
-        echo '<br>';
-        echo 'nome: '.$hotel['name'] .'<br>'. 'descrizione: '.$hotel['description'] .'<br>'. 'parcheggio: '.($hotel['parking'] ? 'si' : 'no') .'<br>'. 'voto: '.$hotel['vote'] .'<br>'. 'distanza dal centro: '.$hotel['distance_to_center'].' km <br>';
-    }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NOME HOTEL</th>
+      <th scope="col">DESCRIZIONE</th>
+      <th scope="col">PARCHEGGIO</th>
+      <th scope="col">VOTO</th>
+      <th scope="col">DISTANZA DAL CENTRO</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($hotels as $hotel ){ ?>
+    <tr>
+      <td><?php echo $hotel['name'] ?></td>
+      <td><?php echo $hotel['description'] ?></td>
+      <td><?php echo $hotel['parking'] ? 'si' : 'no' ?></td>
+      <td><?php echo $hotel['vote'] ?></td>
+      <td><?php echo $hotel['distance_to_center'].' km' ?></td>
+    </tr>
+    <?php } ?>
+  </tbody>
+</table>
+</body>
+</html>
