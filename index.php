8ac2a4b06baa18e4cf574f5b8cc8920ee6ef519b
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-25 17:44:28 +0100

    print array data

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    foreach($hotels as $hotel ){
+        echo '<br>';
+        echo 'nome: '.$hotel['name'] .'<br>'. 'descrizione: '.$hotel['description'] .'<br>'. 'parcheggio: '.($hotel['parking'] ? 'si' : 'no') .'<br>'. 'voto: '.$hotel['vote'] .'<br>'. 'distanza dal centro: '.$hotel['distance_to_center'].' km <br>';
+    }
+
+?>
